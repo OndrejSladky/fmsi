2230770c12e8e91130b868617e2d6d86eb9ddb42
OndrejSladky <ondra.sladky@seznam.cz>
2024-08-11 14:13:31 +0200

    Revert "FMSI supports masked superstrings longer than 2^31 chars."
    This reverts commit 3fa4f66b651cb3e1293b528f7006d087ab8045af.

--- src/parser.h
+++ src/parser.h
@@ -1,33 +1,33 @@
 #pragma once
 
+#include "kseq.h"
 #include <stdexcept>
 #include <stdio.h>
 #include <string>
+#include <zlib.h>
 #include <vector>
+
+KSEQ_INIT(gzFile, gzread)
 
 /// Load masked superstring from the fasta file in the mask-cased format.
 std::string read_masked_superstring(std::string fn) {
-    std::vector<char> masked_superstring;
-    std::ifstream fasta(fn);
-    if (fasta.is_open()) {
-        char c;
-        bool readingHeader = false;
-        while (fasta >> std::noskipws >> c) {
-            if (c == '>') {
-                readingHeader = true;
-            }
-            else if (c == '\n') readingHeader = false;
-            if (readingHeader) continue;
-            // Disregard white space.
-            if (c == '\n' || c == '\r' || c == ' ') continue;
-            if (c != 'A' && c != 'C' && c != 'G' && c != 'T' && c != 'a' && c != 'c' && c != 'g' && c != 't') {
-                throw std::invalid_argument("invalid character in masked superstring");
-            }
-            masked_superstring.push_back(c);
-        }
-        fasta.close();
-    } else {
-        throw std::invalid_argument("couldn't open file " + fn);
+  gzFile fp;
+  kseq_t *seq;
+
+  fp = gzopen(fn.c_str(), "r");
+  if (fp == 0) {
+    throw std::invalid_argument("couldn't open file " + fn);
+  }
+
+  seq = kseq_init(fp);
+    std::vector<char> ret;
+  int l;
+  while ((l = kseq_read(seq)) >= 0) {
+    for (int i = 0; i < l; ++i) {
+      ret.push_back(seq->seq.s[i]);
     }
-    return std::string(masked_superstring.begin(), masked_superstring.end());
+  }
+  kseq_destroy(seq);
+  gzclose(fp);
+  return std::string(ret.begin(), ret.end());
 }
