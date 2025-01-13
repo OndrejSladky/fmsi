38208c2535f127a143427baa379bafcea70d650d
OndrejSladky <ondra.sladky@seznam.cz>
2025-01-13 12:12:22 +0100

    Integration tests for hash queries.

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -11,6 +11,7 @@ $PROG index $TESTS/integration_b.fa
 $PROG merge -p $TESTS/integration_a.fa -p $TESTS/integration_b.fa -r $BIN/merged.fa
 
 $PROG query -k 3 -q $TESTS/queries.txt $TESTS/integration_a.fa > $BIN/a.txt 2> /dev/null
+$PROG query -k 3 -q $TESTS/queries.txt -H $TESTS/integration_a.fa > $BIN/a_hash.txt 2> /dev/null
 $PROG query -k 3 -q $TESTS/queries.txt -f xor $TESTS/integration_a.fa > $BIN/a_xor.txt 2> /dev/null
 $PROG query -k 3 -q $TESTS/queries.txt $TESTS/integration_b.fa > $BIN/b.txt 2> /dev/null
 $PROG query -k 3 -q $TESTS/queries.txt -f xor $TESTS/integration_b.fa > $BIN/b_xor.txt 2> /dev/null
@@ -24,6 +25,8 @@ $PROG normalize -k 3 -s $BIN/merged.fa > $BIN/merged_normalized2.fa 2> /dev/null
 
 diff $TESTS/result_a_complements.txt $BIN/a.txt || exit 1
 echo "a.txt OK"
+diff $TESTS/result_a_complements_hash.txt $BIN/a_hash.txt || exit 1
+echo "a_hash.txt OK"
 diff $TESTS/result_b_complements.txt $BIN/b.txt || exit 1
 echo "b.txt OK"
 diff $TESTS/result_b_complements_xor.txt $BIN/b_xor.txt || exit 1
